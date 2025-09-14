6fd573fcb18fc06f761f4a229b0eec442a84b1e0
isa-sk-b <isabelasakomura@gmail.com>
2025-09-14 12:02:33 -0300

    Update strcat.asm

--- Assembly-codes/strcat.asm
+++ Assembly-codes/strcat.asm
@@ -1,10 +1,26 @@
 # strcat function with assembly 
 		.data
+		.align 0
+str1:		.asciz  "Type first word: "
+str2: 		.asciz 	"Type second word: "
 		.text 
 		.align 2
 		.globl main 
-main: 
+main: 		
+		# Print first message: 
+		la a0,str1 
+		addi a7,zero,4 
+		ecall 
+		
+		# Count size of first string typed by user:
+		la a0,str1 
+		addi a7,,zero,8 # Reuse space: write 1st user string in space allocated to label in .data section 
+		ecall 
+		
+		
+
+		# Print second message: 
+		
 
 
-
-
+read_size: 	
