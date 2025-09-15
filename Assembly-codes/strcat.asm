3273fc8cad4413b65f80ba0090c181da9f960b24
isa-sk-b <isabelasakomura@gmail.com>
2025-09-14 22:43:40 -0300

    Update strcat.asm

--- Assembly-codes/strcat.asm
+++ Assembly-codes/strcat.asm
@@ -1,4 +1,4 @@
-# strcat function with assembly 
+# strcat function - Assembly RISCV
 		.data
 		.align 0
 str1:		.asciz  "Type first word: "
@@ -16,11 +16,18 @@ main:
 		la a0,str1 
 		addi a7,,zero,8 # Reuse space: write 1st user string in space allocated to label in .data section 
 		ecall 
-		
-		
+		add a2,a0,zero 	
+		add a1,zero,zero 
+		jal read_size
+		add s0,a1,zero	
 
 		# Print second message: 
 		
 
-
+		# a2: first address of vector in memory (used to roam through the vector)
+		# a1: counter to number of characters in string, used as 
 read_size: 	
+
+
+
+
